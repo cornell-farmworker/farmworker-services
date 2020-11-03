ba05ead9dae979496cc13d0b5b467019ee52a55f
cfs98 <cfs98@cornell.edu>
2020-11-03 00:16:05 -0700

    Fixed mistake still trying to fix county addition

--- csvmap.js
+++ csvmap.js
@@ -84,8 +84,9 @@ var customLayer = L.geoJson(null, {
       // add "County" to county names (undid to fix issue with adding county after statewide C.S.)
       //if (p == 'county' && v) {
         //v += ' County';
-        //feature.properties[p] = v;;
-      }
+      //feature.properties[p] = v;;
+      //}
+
 
       // add value to fulltext if it is a searched field
       if (csvmap.config.searched_fields.indexOf(p) > -1) {
